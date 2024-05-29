b58ef2f3a491e338d76531d9e1805b5f6bacbefd
Danielle Gonçalves Ferreira <160173090+daniferreira05@users.noreply.github.com>
2024-05-29 10:52:51 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+function DesenharCartela(){
+    const pai_div_cartela = document.getElementById('body_cartelas');
+    const div_cartelas = document.createElemente('div');
+    div_cartelas.className = 'cartela';
+}
